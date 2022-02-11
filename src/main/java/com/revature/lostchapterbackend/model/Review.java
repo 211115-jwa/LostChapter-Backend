996d087d6818a307f98855da6d5e01541d807ab9
fponce21 <ponce21francisco@gmail.com>
2022-02-11 12:20:56 -0800

    review controller tests finsihed excepte update

--- src/main/java/com/revature/lostchapterbackend/model/Review.java
+++ src/main/java/com/revature/lostchapterbackend/model/Review.java
@@ -74,7 +74,7 @@ public class Review {
 		this.ratingOne = 0;
 		this.ratingTwo = 0;
 		this.ratingThree = 0;
-		this.sentAt = LocalDateTime.now();
+		this.sentAt = null;
 	}
 	
 	public double getOverallRating() {
