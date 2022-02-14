f74f8e1feebfbdd7f3dfeba88f8ab15f59d6a5f9
Dana Jenkins <DanaQuinnJenkins@gmail.com>
2022-02-14 09:11:36 -0800

    Update ReviewController.java

--- src/main/java/com/revature/lostchapterbackend/controller/ReviewController.java
+++ src/main/java/com/revature/lostchapterbackend/controller/ReviewController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,7 +15,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.revature.lostchapterbackend.exceptions.BookNotFoundException;
 import com.revature.lostchapterbackend.exceptions.InvalidParameterException;
@@ -22,6 +27,7 @@ import com.revature.lostchapterbackend.exceptions.ReviewNotFoundException;
 import com.revature.lostchapterbackend.model.Review;
 import com.revature.lostchapterbackend.service.BookService;
 import com.revature.lostchapterbackend.service.ReviewService;
+import com.revature.lostchapterbackend.JWT.TokenProvider;
 
 @RestController
 @CrossOrigin("*")
@@ -30,6 +36,7 @@ public class ReviewController {
 
 	private static ReviewService reviewService;
 	private static BookService bookService;
+	private TokenProvider tokenProvider;
 	
 	public ReviewController() {
 		super();
@@ -42,16 +49,18 @@ public class ReviewController {
 	}
 
 	@GetMapping(path = "/reviews")
-	public ResponseEntity<List<Review>> getAllReviews() {
+	public ResponseEntity<List<Review>> getAllReviews(@RequestHeader("Authorization") String authorization) {
 		logger.info("ReviewController.getAllReviews() invoked.");
+
 		List<Review> allReviews = reviewService.getAllReviews();
 		return ResponseEntity.ok(allReviews);
 	}
 
 	@GetMapping(path = "/reviews/{reviewId}")
-	public ResponseEntity<Object> getReviewById(@PathVariable String reviewId) 
+	public ResponseEntity<Object> getReviewById(@PathVariable String reviewId, @RequestHeader("Authorization") String authorization) 
 			throws ReviewNotFoundException {
 		logger.info("ReviewController.getReviewById() invoked.");
+
 		try {
 			Review review = reviewService.getReviewById(reviewId);
 			return ResponseEntity.ok(review);
@@ -61,27 +70,33 @@ public class ReviewController {
 	}
 
 	@PostMapping(path = "/reviews")
-	public ResponseEntity<Object> postNewReview(@RequestBody Review newReview) throws InvalidParameterException {
+	public ResponseEntity<Object> postNewReview(@RequestBody Review newReview, @RequestHeader("Authorization") String authorization) 
+			throws InvalidParameterException {
 		logger.info("ReviewController.postNewReview() invoked.");
-
+		String token = tokenProvider.extractToken(authorization);
+		HttpHeaders jwtHeader = tokenProvider.getHeaderJWT(token);
 		if (newReview != null) {
 			newReview.setSentAt(LocalDateTime.now());
 			reviewService.addReview(newReview);
-			return ResponseEntity.status(HttpStatus.CREATED).build();
+			return new ResponseEntity<Object>(newReview, jwtHeader, HttpStatus.CREATED);
 		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		return new ResponseEntity<>(jwtHeader, HttpStatus.BAD_REQUEST);
 	}
 
 	@PutMapping(path = "/reviews/{reviewId}")
-	public ResponseEntity<Object> updateReviewById(@PathVariable String reviewId, @RequestBody Review updatedReview)
+	public ResponseEntity<Object> updateReviewById(@PathVariable String reviewId,
+			@RequestBody Review updatedReview, @RequestHeader("Authorization") String authorization)
 			throws ReviewNotFoundException, InvalidParameterException {
 		logger.info("ReviewController.updateReviewById() invoked.");
-
+		
+		String token = tokenProvider.extractToken(authorization);
+		HttpHeaders jwtHeader = tokenProvider.getHeaderJWT(token);
+		
 		try {
 			reviewService.updateReview(updatedReview, reviewId);
-			return ResponseEntity.ok(updatedReview);
+			return new ResponseEntity<>(updatedReview, jwtHeader, HttpStatus.OK);
 		} catch (ReviewNotFoundException e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+			return new ResponseEntity<>(jwtHeader, HttpStatus.BAD_REQUEST);
 		}
 	}
 
@@ -89,9 +104,10 @@ public class ReviewController {
 	public ResponseEntity<Object> getAllReviewsForBook(@PathVariable int bookId) 
 			throws BookNotFoundException {
 		logger.info("ReviewController.getAllReviewsForBook() invoked.");
+
 		try {
 			List<Review> reviewList = reviewService.getReviewsByBook(bookService.getBookById(bookId));
-			return ResponseEntity.ok(reviewList);
+			return new ResponseEntity<>(reviewList, HttpStatus.OK);
 		} catch (BookNotFoundException e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		}
